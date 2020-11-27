9b6a64da5feffddc29bb437faa0c0e7a8b6bfd04
Red Fox <ayartokoev3@gmail.com>
2020-11-27 18:51:04 +0300

    Update __init.py__.py

--- project_kurs/version_3/photo_data/__init.py__.py
+++ project_kurs/version_3/photo_data/__init.py__.py
@@ -1,3 +1,3 @@
 from photo_data.core import *
 
-find_element_in_browser("собаки машины", 200, "C:\\Users\\Red_Fox\\Pictures", 70, 20)
+find_element_in_browser("собаки машины", 200, "C:\\Users\\Red_Fox\\Pictures", 70, 30)
