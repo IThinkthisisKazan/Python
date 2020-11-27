1d7e930c545120d91a216da631abe68ae4a919d2
Red Fox <ayartokoev3@gmail.com>
2020-11-27 16:56:51 +0300

    Final
    Final version of the term paper

---
+++ project_kurs/version_3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Ayar Tokoev",
+    author_email="ayartokoev3@gmail.com",
+    description="The package is designed to search for pictures on the Internet and separate them",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
